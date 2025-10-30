c527173e259f1a4c74659402bd61bdc975ea4fb5
vanathy-meera <vidyameera93@gmail.com>
2025-10-30 21:14:06 +0530

    Add files via upload
    Week 1 Day 1

---
+++ week1/day1/EmployeeDetails.java
@@ -0,0 +1,31 @@
+package week1.day1;
+
+public class EmployeeDetails 
+{
+	
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+	
+		String ename = "Vanathy Meera";
+		short age = 45;
+		double salary = 23899.923456789; 
+		char gender = 'F';
+		boolean acceptTerms= false;
+		float rating = 3.123456789f;
+		long mobileNumber = 9876543210L;
+		int pinCode = 600001;
+		byte luckyNumber = 9;
+		
+		System.out.println("Employee Details");
+		System.out.println("Employee Name:"+ename);
+		System.out.println("Employee Age:"+age);
+		System.out.println("Employee Gender:"+gender);
+		System.out.println("Employee Phone No:"+mobileNumber);
+		System.out.println("Employee Pincode:"+pinCode);
+		System.out.println("Lucky Number:"+luckyNumber);
+		System.out.println("Employee Salary:"+salary);
+		System.out.print("Employee Rating:"+rating);
+		System.out.println(" Accept Terms:"+acceptTerms);
+     }
+}
